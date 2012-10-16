9cde29b64a1df25db1a0af35a8264d4ee26c8526
subash adhikari <subash.adikari@gmail.com>
2012-10-17 10:28:11 +1100

    added compare date function
    This function returns the date comparision between the current time and
    the time tweet was posted.
    For example: '1 hour ago'

--- tweetfeed.jquery.js
+++ tweetfeed.jquery.js
@@ -86,6 +86,30 @@ if ( typeof Object.create !== 'function' ) {
 		},
 
 		/**
+		 * compares the given date to the current date
+		 * @param  {String} time time representation
+		 * @return {String}      compared time 
+		 */
+		compareDates: function(time){
+			var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
+				diff = (((new Date()).getTime() - date.getTime()) / 1000),
+				day_diff = Math.floor(diff / 86400);
+			
+			if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
+				return;
+			
+			return day_diff == 0 && (
+				diff < 60 && "just now" ||
+				diff < 120 && "1 minute ago" ||
+				diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+				diff < 7200 && "1 hour ago" ||
+				diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+				day_diff == 1 && "Yesterday" ||
+				day_diff < 7 && day_diff + " days ago" ||
+				day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+		},
+
+		/**
 		 * Regex to convert links in tweets to anchor tags
 		 * @param  {String} text String to be linkified
 		 * @return {HTML}      converted anchor tags
